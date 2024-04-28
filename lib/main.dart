624c12eb02e2614e4576a7504362028c464159ba
mahmoud-star680 <113873810+mahmoud-star680@users.noreply.github.com>
2024-04-28 13:41:02 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:cp_news/pages/landing_page.dart';
 import 'package:flutter/material.dart';
+import 'package:cp_news/pages/landing_page.dart';
 
 void main() {
   runApp(const MyApp());
